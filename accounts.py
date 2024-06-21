63398bdb8616a19bd767408005267fc42b4b4195
TotalAwesome <39047158+TotalAwesome@users.noreply.github.com>
2024-06-21 14:53:57 +0300

    Update accounts.py

--- accounts.py
+++ accounts.py
@@ -1,9 +1,10 @@
+TELEGRAM_ACCOUNTS = [
+    dict(phone='+99999999999', proxy="user:pass@host:port"),
+    dict(phone='+99999999991', proxy="user:pass@host:port"),
+    dict(phone='+55555555544'),
+]
 try:
     from accounts_local import *
 except ImportError:
+    pass
 
-    TELEGRAM_ACCOUNTS = [
-        dict(phone='+99999999999', proxy="user:pass@host:port"),
-        dict(phone='+99999999991', proxy="user:pass@host:port"),
-        dict(phone='+55555555544'),
-    ]
