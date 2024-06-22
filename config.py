95a4a8da96a2eeaae7164b0d2e4501165fe52892
Viktor Aksyutin <v.aksyutin@spacevm.org>
2024-06-23 01:22:03 +0300

    tg api pair

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 ACCOUNTS_DIR = "accounts_data"
-# TELEGRAM CORE API
-API_ID = 0000
-API_HASH = "000000000000000000000000000"
-try:
-    from config_local import *
-except ImportError:
-    pass
+# TELEGRAM WEB
+TELEGRAM_AUTH = dict(
+    api_id=2496,
+    api_hash="8da85b0d5bfe62527e5b244c209159c3",
+    device_model="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
+    system_version="Windows",
+    app_version="1.28.3 Z",
+    lang_code="en",
+    system_lang_code="en-US",
+)
