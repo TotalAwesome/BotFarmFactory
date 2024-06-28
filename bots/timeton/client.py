a864f1f49f12463133714c43cf582de6a248ec4e
TotalAwesome <39047158+TotalAwesome@users.noreply.github.com>
2024-06-28 14:41:31 +0300

    timeton bonus, referrals, staking claiming

--- bots/timeton/client.py
+++ bots/timeton/client.py
@@ -1,9 +1,8 @@
-from random import random
-from datetime import datetime
 from bots.base.utils import to_localtz_timestamp
 from bots.base.base import BaseFarmer, time
 from bots.timeton.strings import HEADERS, URL_AUTH, URL_INIT, URL_BONUS_CLAIM, URL_FARM_CLAIM, URL_FARM_START, \
-    MSG_BONUS, MSG_CLAIM, MSG_STATE, MSG_FARM
+    MSG_BONUS, MSG_CLAIM, MSG_STATE, MSG_FARM, URL_STAKING_CLAIM, URL_FRIENDS_CLAIM, MSG_FRIENDS_CLAIM, \
+    MSG_STAKING_CLAIM
 
 class BotFarmer(BaseFarmer):
 
@@ -11,7 +10,6 @@ class BotFarmer(BaseFarmer):
     info = {}
     extra_code = "TotalAwesome"
     initialization_data = dict(peer=name, bot=name, url=URL_INIT)
-    bonus_claimed_date = None
     payload_base = {}
 
     def set_headers(self, *args, **kwargs):
@@ -33,34 +31,60 @@ class BotFarmer(BaseFarmer):
             return {}
 
     def set_start_time(self):
-        self.start_time = self.claim_date + 5
+        self.start_time = min(self.claim_time, self.ref_claim_time, self.staking_claim_time)
 
     @property
-    def claim_date(self):
-        return to_localtz_timestamp(self.info['claimDate'])
+    def claim_time(self):
+        return to_localtz_timestamp(self.info["claimDate"])
 
-    def claim(self):
-        if self.info['claimActive'] and self.claim_date <= time():
+    @property
+    def ref_claim_time(self):
+        return to_localtz_timestamp(self.info["refClaimTime"])
+
+    @property
+    def staking_claim_time(self):
+        return to_localtz_timestamp(self.info["stakingDate"])
+
+    @property
+    def bonus_claim_time(self):
+        return to_localtz_timestamp(self.info["counterDateBonus"])
+
+    def farm_claim(self):
+        if self.info['claimActive'] and self.claim_time <= time():
             if response := self.api_call(URL_FARM_CLAIM, post=False):
                 self.info = response['data']
                 self.log(MSG_CLAIM)
     
+    def ref_claim(self):
+        if self.ref_claim_time <= time():
+            if response := self.api_call(URL_FRIENDS_CLAIM, post=False):
+                self.info = response["data"]
+                self.log(MSG_FRIENDS_CLAIM)
+    
+    def staking_claim(self):
+        if self.staking_claim_time <= time():
+            if response := self.api_call(URL_STAKING_CLAIM, post=False):
+                self.info = response["data"]
+                self.log(MSG_STAKING_CLAIM)
+
+    def claim_bonus(self):
+        if self.bonus_claim_time <= time():
+            if response := self.api_call(URL_BONUS_CLAIM, post=False):
+                self.info = response["data"]
+                self.log(MSG_BONUS)
+            
     def start_farm(self):
         if not self.info['claimActive']:
             if response := self.api_call(URL_FARM_START, post=False):
                 self.info = response['data']
                 self.log(MSG_FARM)
 
-    def claim_bonus(self):
-        today = datetime.now().date()
-        if not self.bonus_claimed_date or today > self.bonus_claimed_date:
-            if result := self.api_call(URL_BONUS_CLAIM, post=False):
-                self.log(MSG_BONUS)
-            self.bonus_claimed_date = today
 
     def farm(self):
         self.claim_bonus()
-        self.claim()
+        self.farm_claim()
         self.start_farm()
+        self.ref_claim()
+        self.staking_claim()
         self.log(MSG_STATE.format(balance=self.info['balance']))
         
