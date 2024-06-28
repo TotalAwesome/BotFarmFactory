1fb0ac26dbffd5c476179c1599c9a783af988abe
TotalAwesome <39047158+TotalAwesome@users.noreply.github.com>
2024-06-28 17:36:59 +0300

    iceberg

---
+++ bots/iceberg/strings.py
@@ -0,0 +1,30 @@
+URL_INIT = "https://0xiceberg.store/webapp/"
+URL_BALANCE = "https://0xiceberg.store/api/v1/web-app/balance/"
+URL_FARMING = "https://0xiceberg.store/api/v1/web-app/farming/" # GET / POST
+URL_TASKS = "https://0xiceberg.store/api/v1/web-app/tasks/"
+URL_CLAIM_FARM = "https://0xiceberg.store/api/v1/web-app/farming/collect/"  # DELETE
+
+MSG_PROFILE = "Обновил профиль"
+MSG_CLAIM = "Собрал нафармленное"
+MSG_FRIENDS_CLAIM = "Собрал нафармленное рефералами"
+MSG_FARM = "Начал фармить"
+MSG_STATE = "Баланс: {balance}"
+
+HEADERS = {
+    'authority': '0xiceberg.store' ,
+    'accept': '*/*' ,
+    'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,cy;q=0.6' ,
+    'cache-control': 'no-cache' ,
+    'content-length': '0' ,
+    'origin': 'https://0xiceberg.store' ,
+    'pragma': 'no-cache' ,
+    'referer': 'https://0xiceberg.store/webapp/' ,
+    'sec-ch-ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"' ,
+    'sec-ch-ua-mobile': '?0' ,
+    'sec-ch-ua-platform': '"Linux"' ,
+    'sec-fetch-dest': 'empty' ,
+    'sec-fetch-mode': 'cors' ,
+    'sec-fetch-site': 'same-origin' ,
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36' ,
+    'x-telegram-auth': '',
+}
