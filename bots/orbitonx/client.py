cf3562ed07ec460e0049c35209b1b052d0c598c9
Viktor Aksyutin <v.aksyutin@spacevm.org>
2024-07-28 22:55:46 +0300

    orbitonx

---
+++ bots/orbitonx/client.py
@@ -0,0 +1,139 @@
+import json
+from time import sleep
+from random import random
+from telethon.types import InputBotAppShortName
+from bots.base.base import BaseFarmer, time
+from bots.base.utils import api_response, to_localtz_timestamp
+from urllib.parse import unquote
+
+from bots.orbitonx.strings import URL_AUTH, URL_INIT, HEADERS, MSG_AUTH_ERROR, URL_INFO, URL_BALANCE, URL_QUESTS, \
+    URL_STAKING_CLAIM, URL_TAP, MSG_STAKING_CLAIMED, MSG_STAKING_STARTED, MSG_STAKING_TAP, URL_TASKS, \
+    MSG_TASK_CLAIMED, URL_TASK_CLAIM, URL_WATCH_AD, MSG_WATCHED_AD, URL_STOCKS
+
+
+class BotFarmer(BaseFarmer):
+
+    name = 'orbitonx_bot'
+    upgrades = {}
+    info = {}
+    levels = {}
+    quests = {}
+    tasks = []
+    next_claim = None
+    app_extra = "friendId102796269"
+    tokens = {}
+    refreshable_token = True
+
+    @property
+    def initialization_data(self):
+        return dict(peer=self.initiator.get_input_entity(self.name), 
+                    app=InputBotAppShortName(self.initiator.get_input_entity(self.name), "orbitonx"),
+                    start_param=self.app_extra)
+
+    def set_headers(self, *args, **kwargs):
+        self.headers = HEADERS.copy()
+        self.get = api_response(super().get)
+        self.post = api_response(super().post)
+        self.delete = api_response(super().delete)
+        self.patch = api_response(super().patch)
+
+    def set_start_time(self):
+        timestamps = (self.portfolio['finishStaking'], self.info['adNextAvailableTime'])
+        self.start_time = min(map(to_localtz_timestamp, timestamps)) + 5
+
+    def prepare_auth_data(self, url):
+        from urllib.parse import unquote, parse_qsl
+        parsed = parse_qsl(unquote(unquote(url)))
+        user = json.loads(parsed[0][-1].split('user=')[-1])
+        parsed.pop(0)
+        parsed = dict(parsed)
+        data = {
+            "tgChatId": user['id'],
+            "webAppInitData": {
+                "user": user,
+                "chat_instance": parsed['chat_instance'],
+                "chat_type": parsed['chat_type'],
+                "auth_date": parsed['auth_date'],
+                "hash": parsed['hash']
+            },
+            "user": user
+        }
+        return data
+        
+
+    def authenticate(self, *args, **kwargs):
+        auth_data = self.initiator.get_auth_data(**self.initialization_data)
+        auth_data = self.prepare_auth_data(auth_data['url'])
+        if response := self.post(URL_AUTH, json=auth_data):
+            self.tokens = response['data']
+            self.headers['Authorization'] = f"Bearer {self.tokens['token']}"
+        else:
+            self.is_alive = False
+            raise Exception(MSG_AUTH_ERROR)
+
+    def sync(self):
+        if response := self.get(URL_INFO):
+            self.info = response['data']
+
+    def update_tasks(self):
+        if response := self.get(URL_TASKS):
+            self.tasks = response['data']['tasks']
+
+    def check_tasks(self):
+        self.update_tasks()
+        for task in self.tasks:
+            if task['status'] == 'not started':
+                response = self.post(URL_TASKS, json={'taskId': task['id']})
+                if response.get('status'):
+                    if self.get(URL_TASK_CLAIM.format(**task)).get('status'):
+                        self.log(MSG_TASK_CLAIMED.format(**task))
+                        sleep(random() * 5)
+
+    def watch_ad(self):
+        while to_localtz_timestamp(self.info['adNextAvailableTime']) < time():
+            if response := self.get(URL_WATCH_AD):
+                if response['data']['balance'] > self.info['balance']:
+                    self.info.update(response['data'])
+                    self.log(MSG_WATCHED_AD)
+                    sleep(random() * 20)
+                else:
+                    break
+
+
+    def update_quests(self):
+        if response := self.get(URL_QUESTS):
+            self.quests.update(response['data'])
+
+    @property
+    def portfolio(self):
+        return self.quests['quest']['portfolios'][0]
+
+    def claim_or_farm(self):
+        self.update_quests()
+        # claim
+        if self.portfolio['finishStaking'] and to_localtz_timestamp(self.portfolio['finishStaking']) < time() and not self.portfolio['active']:
+            if response := self.patch(URL_STAKING_CLAIM, json={}):
+                self.quests = response['data']
+                self.log(MSG_STAKING_CLAIMED.format(**self.quests['quest']))
+                self.update_quests()
+        # tap
+        if self.portfolio['active']:
+            payload = dict(coins=[{'id': coin['id'], 'progress': 100} for coin in self.portfolio['coins']])
+            payload['energy'] = 500
+            response = self.patch(URL_TAP, json=payload)
+            if response:
+                self.log(MSG_STAKING_TAP)
+                self.update_quests()
+    
+    def select_exchange(self):
+        if not self.info['stoke']['id'] not in (8, 9):
+            self.patch(URL_STOCKS, json={'stockId': 9})
+            self.sync()
+
+
+    def farm(self):
+        self.sync()
+        self.select_exchange()
+        self.claim_or_farm()
+        self.check_tasks()
+        self.watch_ad()
---
+++ bots/orbitonx/strings.py
@@ -0,0 +1,65 @@
+URL_INIT = "https://game.orbitonx.com/"
+URL_AUTH = "https://api.orbitonx.com/api/auth"  # POST payload
+URL_BALANCE = "https://api.orbitonx.com/api/users/balance"  # GET
+URL_INFO = "https://api.orbitonx.com/api/users/info"  # GET
+URL_REWARD = "https://api.orbitonx.com/api/users/reward"  # POST None
+URL_STOCKS = "https://api.orbitonx.com/api/general/stocks"  # GET
+URL_QUESTS = "https://api.orbitonx.com/api/user-quests"  # GET
+URL_STAKING_CLAIM = "https://api.orbitonx.com/api/user-coins/collect-reward"  # PATCH
+URL_TAP = "https://api.orbitonx.com/api/user-coins"  # POST payload
+URL_TASKS = "https://api.orbitonx.com/api/user-tasks"  # GET
+URL_TASK_CLAIM = "https://api.orbitonx.com/api/user-tasks/get-bonus/{id}"  # GET
+URL_START_TASK = "https://api.orbitonx.com/api/user-tasks"  # POST
+URL_WATCH_AD = "https://api.orbitonx.com/api/users/increase-balance"  # GET LOL
+URL_CARDS_LIST = "https://api.orbitonx.com/api/boost-cards"
+URL_MY_CARDS = "https://api.orbitonx.com/api/user-boost-cards"
+URL_COMBOS = "https://api.orbitonx.com/api/combos"
+URL_STOCKS = "https://api.orbitonx.com/api/general/stocks"
+
+MSG_AUTH_ERROR = "Ошибка аутентификации"
+MSG_STAKING_CLAIMED = "Собрал за стейкинг {rewardFromStacking}"
+MSG_STAKING_STARTED = "Начал стейкать"
+MSG_STAKING_TAP = "Натапал на стейкинг"
+MSG_TASK_CLAIMED = "Выполнил таску на {capacity} $RCT"
+MSG_WATCHED_AD = "Посмотрел рекалму ;) получил 10 $RCT"
+
+HEADERS = {
+  'accept': 'application/json, text/plain, */*',
+  'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,cy;q=0.6',
+#   'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTEzNTU2MCwicm9sZUlkIjoxLCJpYXQiOjE3MjAwMzc1MTksImV4cCI6MTcyMDEyMzkxOX0._f3IudNWw4TUaZGpR7tCZxu2RUfWIRBM11v857wFya0',
+  'cache-control': 'no-cache',
+  'content-type': 'application/json',
+  'origin': 'https://game.orbitonx.com',
+  'pragma': 'no-cache',
+  'priority': 'u=1, i',
+  'referer': 'https://game.orbitonx.com/',
+  'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
+  'sec-ch-ua-mobile': '?0',
+  'sec-ch-ua-platform': '"Windows"',
+  'sec-fetch-dest': 'empty',
+  'sec-fetch-mode': 'cors',
+  'sec-fetch-site': 'same-site',
+  'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+#   'x-local-time': '2024-07-03T20:37:58.317Z',
+#   'x-request-id': 'tzhh2g',
+  'x-timezone': 'Europe/Moscow',
+}
+#   {
+#     "tgChatId":102796269,
+#     "webAppInitData": {"user":{ "id":102796269,
+#                                 "first_name":"Тотально Классная Змея Алабамы",
+#                                 "last_name":"",
+#                                 "username":"TotalAwesome",
+#                                 "language_code":"ru",
+#                                 "allows_write_to_pm":true},
+#                         "chat_instance":"-8420253757687619867",
+#                         "chat_type":"sender",
+#                         "auth_date":"1720039071",
+#                         "hash":"ddb6a39a1de22115e960bb479db01fe88b3cd7e0056656b9bd064569a2031378"},
+#     "user":{"id":102796269,
+#             "first_name":"Тотально Классная Змея Алабамы",
+#             "last_name":"",
+#             "username":"TotalAwesome",
+#             "language_code":"ru",
+#             "allows_write_to_pm":true}
+# }
