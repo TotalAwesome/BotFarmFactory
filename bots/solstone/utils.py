ce67773f0e8a0eef9800681fc4398092d249bc37
Viktor Aksyutin <v.aksyutin@spacevm.org>
2024-07-28 22:52:08 +0300

    cleaning

--- bots/solstone/utils.py
+++ bots/solstone/utils.py
@@ -6,10 +6,3 @@ def utc_timestamp():
     utc_time = now.replace(tzinfo=timezone.utc) 
     return utc_time.timestamp()
   
-
-now = int(utc_timestamp())
-end = 1722083825 + (8 * 60 * 60 + 10)
-print(datetime.datetime.fromtimestamp(now))
-print(datetime.datetime.fromtimestamp(end))
-print((end - now))
-print()
