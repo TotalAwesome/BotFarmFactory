240756829735003574cabbc1d86d2320e3227527
Xoctel <lihachevdan@gmail.com>
2024-08-22 18:47:17 +0300

    Добавлена прокачка Таски Сжигание при полной прокачке Присоединение к ГИ

--- bots/zavod/strings.py
+++ bots/zavod/strings.py
@@ -10,8 +10,6 @@ URL_CLAIM_MISSION = 'https://zavod-api.mdaowallet.com/missions/claim/'
 URL_CONFIRM_LINK_MISSION = 'https://zavod-api.mdaowallet.com/missions/confirm/link/'
 URL_CONFIRM_TELEGRAM_MISSION = 'https://zavod-api.mdaowallet.com/missions/confirm/telegram/'
 
-
-
 MSG_CLAIM = 'Забрали награду'
 MSG_PROFILE = 'Обновили профиль'
 MSG_STATE = 'Баланс: {balance}'
@@ -27,8 +25,6 @@ MSG_CLAIMED_MISSION = 'Получили {prize} за {name}'
 MSG_LINK_MISSION = 'Делаем задание на {prize} за {name}'
 MSG_TELEGRAM_MISSION = 'Выполняем задание {name}'
 
-
-
 MSG_ERROR_UPGRADING_TOOLKIT = 'Ошибка обновления инструментов: {error}'
 MSG_ERROR_UPGRADING_WORKBENCH = 'Ошибка обновления верстака: {error}'
 MSG_ERROR_BURNING_TOKENS = 'Ошибка сжигания монет: {error}'
@@ -37,23 +33,21 @@ MSG_ERROR_CLAIMING_MISSION = 'Ошибка получения задания {id
 MSG_ERROR_CONFIRMING_LINK_MISSION = 'Ошибка подтверждения задания {id}: {error}'
 MSG_ERROR_CONFIRMING_TELEGRAM_MISSION = 'Ошибка подтверждения задания {id}: {error}'
 
-
-
 HEADERS = {
 
-  'authority': 'zavod-api.mdaowallet.com',
-  'accept': 'application/json, text/plain, */*',
-  'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,cy;q=0.6',
-  'cache-control': 'no-cache',
-  'origin': 'https://zavod.mdaowallet.com',
-  'pragma': 'no-cache',
-  'referer': 'https://zavod.mdaowallet.com/',
-  'sec-ch-ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
-  'sec-ch-ua-mobile': '?0',
-  'sec-ch-ua-platform': '"Linux"',
-  'sec-fetch-dest': 'empty',
-  'sec-fetch-mode': 'cors',
-  'sec-fetch-site': 'same-site',
-  'telegram-init-data': '',
-  'user-agent': '',
+    'authority': 'zavod-api.mdaowallet.com',
+    'accept': 'application/json, text/plain, */*',
+    'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,cy;q=0.6',
+    'cache-control': 'no-cache',
+    'origin': 'https://zavod.mdaowallet.com',
+    'pragma': 'no-cache',
+    'referer': 'https://zavod.mdaowallet.com/',
+    'sec-ch-ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Linux"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-site',
+    'telegram-init-data': '',
+    'user-agent': '',
 }
