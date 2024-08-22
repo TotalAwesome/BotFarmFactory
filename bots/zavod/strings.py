6b1378b404b10a3083432e82db9dfdb1ae8d5d86
Xoctel <lihachevdan@gmail.com>
2024-08-22 10:03:19 +0300

    Add urls

--- bots/zavod/strings.py
+++ bots/zavod/strings.py
@@ -2,8 +2,18 @@ URL_INIT = "https://zavod.mdaowallet.com/"
 URL_PROFILE = "https://zavod-api.mdaowallet.com/user/profile"  # GET
 URL_FARM = "https://zavod-api.mdaowallet.com/user/farm"  # POST
 URL_CALIM = "https://zavod-api.mdaowallet.com/user/claim"  # GET
+URL_GUILDS_JOIN = "https://zavod-api.mdaowallet.com/guilds/join"
+URL_USER_FARM = "https://zavod-api.mdaowallet.com/user/farm"
+URL_UPGRADE_TOOLKIT = "https://zavod-api.mdaowallet.com/user/upgradeToolkit"
+URL_UPGRADE_WORKBENCH = "https://zavod-api.mdaowallet.com/user/upgradeWorkbench"
 
-
+MSG_JOIN_GUILD = "Joined the guild."
+MSG_UPGRADE_TOOLKIT = "Upgraded toolkit."
+MSG_UPGRADE_WORKBENCH = "Upgraded workbench."
+MSG_NO_GUILD = "No guild found."
+MSG_LOG_COINS = "Coins: {tokens}"
+MSG_LOG_TOOLKIT_LEVEL = "Toolkit level: {toolkit_level}"
+MSG_LOG_WORKBENCH_LEVEL = "Workbench level: {workbench_level}"
 MSG_PROFILE = "Обновил профиль"
 MSG_CLAIM = "Склеймил и начал фармить"
 MSG_STATE = "Баланс: {balance}"
