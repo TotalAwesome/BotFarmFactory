e442ba27430754c0f1418caf7c7c06797cdc7723
SoloWay <33679911+SoloWay9@users.noreply.github.com>
2024-09-05 21:29:44 +0300

    update promo list

--- bots/hamster_kombat/strings.py
+++ bots/hamster_kombat/strings.py
@@ -70,12 +70,14 @@ GET_PROMO_HEADERS = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/
 
 PROMO_TOKENS = {
     # promo_id : app_token
-    '43e35910-c168-4634-ad4f-52fd764a843f': 'd28721be-fd2d-4b45-869e-9f253b554e50', # BIKE
-    'fe693b26-b342-4159-8808-15e3ff7f8767': '74ee0b5b-775e-4bee-974f-63e7f4d5bacb', # CLONE
     'b4170868-cef0-424f-8eb9-be0622e8e8e3': 'd1690a07-3780-4068-810f-9b5bbf2931b2', # CUBE
     'c4480ac7-e178-4973-8061-9ed5b2e17954': '82647f43-3f87-402d-88dd-09a90025313f', # TRAIN
     'dc128d28-c45b-411c-98ff-ac7726fbaea4': '8d1cc2ad-e097-4b86-90ef-7a27e19fb833', # MERGE
     '61308365-9d16-4040-8bb0-2f4a4c69074c': '61308365-9d16-4040-8bb0-2f4a4c69074c', # TWERK
     '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71': '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71', # POLY
-    'bc0971b8-04df-4e72-8a3e-ec4dc663cd11': 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11', # Cafe Dash
+    'ef319a80-949a-492e-8ee0-424fb5fc20a6': 'ef319a80-949a-492e-8ee0-424fb5fc20a6', # TRIM
+    'b2436c89-e0aa-4aed-8046-9b0515e1c46b': 'b2436c89-e0aa-4aed-8046-9b0515e1c46b', # ZooPolis
+    '112887b0-a8af-4eb2-ac63-d82df78283d9': '112887b0-a8af-4eb2-ac63-d82df78283d9', # Fluff Crusade
+    'e68b39d2-4880-4a31-b3aa-0393e7df10c7': 'e68b39d2-4880-4a31-b3aa-0393e7df10c7', # Tile Trio
+    '04ebd6de-69b7-43d1-9c4b-04a6ca3305af': '04ebd6de-69b7-43d1-9c4b-04a6ca3305af', # Stone Age
 }
